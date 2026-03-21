2eab6a8e58f6c33e561f56ef4cbc778cda014f3a
Thomas Hørning Biehe <3.worldrecord@gmail.com>
2026-03-21 20:56:38 +0100

    asdsad

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import customtkinter as ctk
 
 root = ctk.CTk()
-root.resizable(False, False)
+#root.resizable(False, False)
 root.geometry("800x800")
+
+diceResultLabel = ctk.CTkLabel(root)
+diceResultLabel.place(relx=0.5, rely=0.5, anchor="center")
+
+typingDisplayFrame = ctk.CTkFrame(root)
+typingDisplayFrame.place(relx=0.5, rely=0.5)
+typingDisplayFrame.config()
 
 root.mainloop()
