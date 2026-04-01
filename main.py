2b9f58e51341ef10915ece6626c400da59483dae
Thomas Biehe <3.worldrecord@gmail.com>
2026-04-01 15:21:40 +0200

    Huge overhaul. Needs one more commit.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from enum import Enum
 import os
 from dotenv import load_dotenv
 from copy import deepcopy
+import re
 
 load_dotenv()
 
@@ -32,13 +33,32 @@ with open(CONFIG_PATH, "r") as f:
 def CreateDefaultValue(parent: any, key: str, defaultValue: any):
     parent[key] = parent[key] if key in parent else defaultValue
 
+def CreateDefaultDiceConfig(diceConfig):
+    print(diceConfig)
+    for i in range(max([len(diceConfig["dice"]), len(diceConfig["bonus"]), len(diceConfig["advantage"])])):
+        if i >= len(diceConfig["dice"]): diceConfig["dice"].append(diceConfig["dice"][-1])
+        if i >= len(diceConfig["bonus"]): diceConfig["bonus"].append(diceConfig["bonus"][-1])
+        if i >= len(diceConfig["advantage"]): diceConfig["advantage"].append(diceConfig["advantage"][-1])
+    print(diceConfig)
+
+            
+
+def GetDiceFromDiceConfig(diceConfigValue: list[int]):
+    diceGroup: list[list[int]] = []
+    for i, value in enumerate(diceConfigValue):
+        nestedI = i % 2
+        if nestedI == 0: diceGroup.append([])
+        diceGroup[-1].append(diceConfigValue[i])
+    return diceGroup
+
 
 #region SET VALUES TO THEIR DEFAULT IF NOT SET IN CONFIG
 for name in config["settings"]["binds"]:
     for c in config[name]:
-        CreateDefaultValue(c, "advantage", 0)
-        CreateDefaultValue(c, "bonus", 0)
-        CreateDefaultValue(c, "dice", [1,20])
+        CreateDefaultValue(c, "advantage", [0])
+        CreateDefaultValue(c, "bonus", [0])
+        CreateDefaultValue(c, "dice", [[1,20]])
+        CreateDefaultDiceConfig(c)
 CreateDefaultValue(config["settings"], "animation", "RANDOM_NUMBERS")
 CreateDefaultValue(config["settings"], "animationSpeed", 1.25)
 CreateDefaultValue(config["settings"], "searchType", "FLEXIBLE")
@@ -61,24 +81,33 @@ root.bind("<Key>", KeyPressed)
 
 def DirectionParse(startIndex, parsingText, endLetters, parseLeft = False, defaultValue: str = "0"):
     result = ""
+    if startIndex == -1:
+        return defaultValue
     i = startIndex + (-1 if parseLeft else 1)
     if i >= 0:
-        while i < len(parsingText) and parsingText[i] not in endLetters:
+        while i < len(parsingText) and i > -1 and parsingText[i] not in endLetters:
             if parseLeft:
                 result = parsingText[i] + result
             else:
-                result += parsingText[i]
-            
+                result += parsingText[i]   
             i += (-1 if parseLeft else 1)
     if result == "":
         result = defaultValue
     return result
 
+def DirectionParseList(startLetter, parsingText, endLetters, parseLeft = False, defaultValue: str = "0"):
+    indicies = [match.start() for match in re.finditer(startLetter, parsingText)]
+    results = []
+    for index in indicies:
+        results.append(DirectionParse(index, parsingText, endLetters, parseLeft, defaultValue))
+    return results
+
 
 class DiceRoller:
 
-    diceList: list[list[int]] = []
-    result: int = 0
+    # diceGroup, AdvantageRolls, Dice[2]
+    diceList: list[list[list[int]]] = []
+    result: list[int] = 0
 
     diceResultLabel: ctk.CTkLabel
     root: ctk.CTk
@@ -140,25 +169,50 @@ class DiceRoller:
                 self.TextFadeInUpdateLoop()
             case "RANDOM_NUMBERS":
                 self.TextRandomNumbersLoop()
-
+    
+    
     def RollDice(self, diceConfig):
+
         self.diceList = []
-        diceResults = []
-        self.result = 0
-        for _ in range(abs(diceConfig["advantage"])+1):
+        self.result = []
+
+        for i, diceConfigGroup in enumerate(diceConfig["dice"]):
             self.diceList.append([])
-            diceResults.append(0)
-            for i in range(diceConfig["dice"][0]):
-                for i in range(random.randint(1,10)):
-                    random.randint(1,diceConfig["dice"][1])
-                self.diceList[-1].append(random.randint(1,diceConfig["dice"][1]))
-                diceResults[-1] += self.diceList[-1][-1]
-            diceResults[-1] += diceConfig["bonus"]
-        if diceConfig["advantage"] > 0:
-            self.result = max(diceResults)
-        else:
-            self.result = min(diceResults)
+            advantageResult = 0
+            advantage = diceConfig["advantage"][i]
+            bonus = diceConfig["bonus"][i]
+            for j in range(abs(advantage)+1):
+                self.diceList[-1].append([])
+                diceGroup = GetDiceFromDiceConfig(diceConfigGroup)
+                groupResult = 0
+                for dice in diceGroup:
+                    for _ in range(dice[0]):
+                        randomValue = random.randint(1, dice[1])
+                        groupResult += randomValue
+                        self.diceList[-1][-1].append(randomValue)
+                groupResult += bonus
+                    
+                
+                advantageResult = groupResult if j == 0 else max(advantageResult, groupResult) if advantage > 0 else min(advantageResult, groupResult)
+            self.result.append(advantageResult)
+            
+        
         self.StartAnimation()
+        # diceResults = []
+        # for _ in range(abs(diceConfig["advantage"])+1):
+        #     self.diceList.append([])
+        #     diceResults.append(0)
+        #     for i in range(diceConfig["dice"][0]):
+        #         for i in range(random.randint(1,10)):
+        #             random.randint(1,diceConfig["dice"][1])
+        #         self.diceList[-1].append(random.randint(1,diceConfig["dice"][1]))
+        #         diceResults[-1] += self.diceList[-1][-1]
+        #     diceResults[-1] += diceConfig["bonus"]
+        # if diceConfig["advantage"] > 0:
+        #     self.result = max(diceResults)
+        # else:
+        #     self.result = min(diceResults)
+        # self.StartAnimation()
         print("rolled dice with config:\n"+diceConfig.__str__())
 
     
@@ -284,18 +338,53 @@ class SearchManager:
                 self.CustomDiceSearch()
 
 
-    
 
+    #TODO make better
     def CustomDiceSearch(self):
         self.currentSearchLabel.configure(text_color="purple")
         try:
-            bonusText = DirectionParse(self.currentSearch.find("b"), self.currentSearch, ".db", True, "0")
-            diceCountText = DirectionParse(self.currentSearch.find("d"), self.currentSearch, ".db", True, "1")
-            diceText = DirectionParse(self.currentSearch.find("d"), self.currentSearch, ".db", False, "4")
-            self.matchingListDiceConfigs = [{"name":"custom", "bonus":int(bonusText), "dice":[int(diceCountText), int(diceText)], "advantage": 0}]
+            splitSearch = self.currentSearch.split(".")
+            splitSearch.pop(0)
+
+            bonusList: list[int] = []
+            diceList: list[list[int]] = []
+            advantageList: list[int] = []
+            
+            for search in splitSearch:
+                diceInterp: list[int] = []
+                bonusInterp: int = 0
+                advantageInterp: int = 0
+
+                bonusText = DirectionParseList("b", search, ".,dba", True, "0")
+                diceCountText = DirectionParseList("d", search, ".,dba", True, "1")
+                diceText = DirectionParseList("d", search, ".,dba", False, "4")
+                advantageText = DirectionParseList("a", search, ".,dba", False, "0")
+
+
+                bonusInterp = sum([int(x) for x in bonusText])
+                advantageInterp = sum([int(x) for x in advantageText])
+                for i, dice in enumerate(diceText):
+                    diceInterp.append(int(diceCountText[i]))
+                    diceInterp.append(int(dice))
+                    pass
+                if len(diceInterp) == 0: diceInterp = [1,4]
+                bonusList.append(bonusInterp)
+                diceList.append(diceInterp)
+                advantageList.append(advantageInterp)
+                
+                #advantageList = [int(x) for x in bonusText]
+
+                # bonusText = DirectionParse(search.find("b"), search, ".dbaq", True, "0")
+                # diceCountText = DirectionParse(search.find("d"), search, ".dbaq", True, "1")
+                # diceText = DirectionParse(search.find("d"), search, ".dbaq", False, "4")
+                # advantageText = DirectionParse(search.find("a"), search, ".dbaq", False, "null")
+            self.matchingListDiceConfigs = [{"name":"custom", "bonus":bonusList, "dice":diceList, "advantage": advantageList}]
+            
+            
         except:
             print("incorrect syntax")
-            self.matchingListDiceConfigs = [{"name":"syntax error", "bonus":0, "dice":[1, 4], "advantage": 0}]
+            self.matchingListDiceConfigs = [{"name":"syntax error", "bonus":[0], "dice":[[1, 4]], "advantage": [0]}]
+        #self.matchingListDiceConfigs = [{"name":"custom", "bonus":int(bonusText), "dice":[int(diceCountText), int(diceText)], "advantage": 0}]
         
         self.UpdateDiceConfigGraphic()
 
@@ -324,18 +413,21 @@ class SearchManager:
             self.currentSearchLabel.configure(text_color="green")
             self.UpdateDiceConfigGraphic()
             
-
+    
     def UpdateDiceConfigGraphic(self):
         diceConfig = self.matchingListDiceConfigs[0]
         text = ""
         text += F"name: {diceConfig["name"]}\n"
         if diceConfig["bonus"] != 0:
             text += F"bonus: {diceConfig["bonus"]}\n"
-        text += F"dice: {diceConfig["dice"][0]}d{diceConfig["dice"][1]}\n"
-        if diceConfig["advantage"] > 0:
-            text += F"advantage: {diceConfig["advantage"]}\n"
-        elif diceConfig["advantage"] < 0:
-            text += F"disadvantage: {-diceConfig["advantage"]}\n"
+        text += "dice: "
+        for diceGroup in diceConfig["dice"]:
+            text += "["
+            for i in range(0,len(diceGroup)-1,2):
+                text += F"{diceGroup[i]}d{diceGroup[i+1]},"
+            text = text.strip(",")+"]"
+        text += "\n"
+        text += F"advantage: {diceConfig["advantage"]}\n"
         self.searchedDiceConfigLabel.configure(text=text)
 
             
@@ -367,10 +459,18 @@ class SearchManager:
             self.ResetFrame()
         elif event.keysym == "BackSpace":
             self.RemoveLetter()
-        elif event.keysym == "period":
-            self.currentSearch = ""
-            self.state = SearchManagerState.customDice
-            self.AddLetter(".")
+        elif event.keysym == "period" or (event.keysym == "minus" and self.state == SearchManagerState.customDice):
+            if self.state != SearchManagerState.customDice:
+                self.currentSearch = ""
+                self.state = SearchManagerState.customDice
+            match event.keysym:
+                case "period":
+                    self.AddLetter(".")
+                case "minus":
+                    self.AddLetter("-")
+        elif event.keysym == "comma":
+            if self.state == SearchManagerState.customDice:
+                self.AddLetter(",")
         elif event.keysym == "plus" or event.keysym == "asterisk" or event.keysym == "slash" or event.keysym == "minus" or event.keysym == "equal":
             self.currentSearch = ""
             self.state = SearchManagerState.additionalBonus
@@ -385,13 +485,13 @@ class SearchManager:
                     self.AddLetter("-")
                 case "equal":
                     self.AddLetter("=")
-        elif event.keysym == "Right":
+        elif event.keysym == "Right" or event.keysym == "Left":
             if len(self.matchingListDiceConfigs) == 0: return
-            self.matchingListDiceConfigs[0]["advantage"] += 1
-            self.UpdateDiceConfigGraphic()
-        elif event.keysym == "Left":
-            if len(self.matchingListDiceConfigs) == 0: return
-            self.matchingListDiceConfigs[0]["advantage"] -= 1
+            for i in range(len(self.matchingListDiceConfigs[0]["advantage"])):
+                #adv = adv + 1 if event.keysym == "Right" else adv - 1
+                self.matchingListDiceConfigs[0]["advantage"][i] = self.matchingListDiceConfigs[0]["advantage"][i] + 1 if event.keysym == "Right" else self.matchingListDiceConfigs[0]["advantage"][i] - 1
+                print(self.matchingListDiceConfigs[0]["advantage"][i])
+            
             self.UpdateDiceConfigGraphic()
         else:
             print(event.keysym)
